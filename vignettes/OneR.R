dd98c8d14908791cb8ac446d7a7a3dd820578483
vonjd <r-project@ephorie.de>
2016-07-25 20:49:25 +0200

    some vignette formatting

--- vignettes/OneR.R
+++ vignettes/OneR.R
@@ -44,23 +44,23 @@ prediction <- predict(model_train, data_test)
 ## ------------------------------------------------------------------------
 eval_model(prediction, data_test)
 
-## ---- fig.width=7.15, fig.height=5---------------------------------------
+## ------------------------------------------------------------------------
 data <- iris
 str(data)
 str(bin(data))
 str(bin(data, nbins = 3))
 str(bin(data, nbins = 3, labels = c("small", "medium", "large")))
 
-## Difference between methods "length" and "content"
+## ------------------------------------------------------------------------
 set.seed(1); table(bin(rnorm(900), nbins = 3))
 set.seed(1); table(bin(rnorm(900), nbins = 3, method = "content"))
 
-## Method "clusters"
+## ---- fig.width=7.15, fig.height=5---------------------------------------
 intervals <- paste(levels(bin(faithful$waiting, nbins = 2, method = "cluster")), collapse = " ")
 hist(faithful$waiting, main = paste("Intervals:", intervals))
 abline(v = c(42.9, 67.5, 96.1), col = "blue")
 
-## Missing values
+## ------------------------------------------------------------------------
 bin(c(1:10, NA), nbins = 2, na.omit = FALSE) # adds new level "NA"
 bin(c(1:10, NA), nbins = 2)
 
